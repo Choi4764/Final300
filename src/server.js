a9e158be1570889bb6bf873109ba00c600d01956
Rien3844 <102456478+Rien3844@users.noreply.github.com>
2024-11-18 20:09:28 +0900

    서버 구동
    server.js 작성 및 연관된 부분 작성

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import net from 'net';
+import { PORT, HOST } from './constants/env.js';
+import { onConnection } from './events/onConnection.js';
+import initServer from './init/index.js'; 
+
+const server = net.createServer(onConnection);
+
+initServer()
+    .then(() => {
+        server.listen(PORT, HOST, () => {
+            console.log(`Server is on ${HOST}:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
