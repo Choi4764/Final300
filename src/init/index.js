128393dd0467fe844c8a63a93e231602455e5f0a
Bzarre <32144764@dankook.ac.kr>
2024-11-19 14:14:59 +0900

    Proto load 관련

---
+++ src/init/index.js
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+import { loadProto } from './loadProtos.js';
+
+const initServer = async () => {
+    try {
+      await loadProto();
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+  export default initServer;
+  
