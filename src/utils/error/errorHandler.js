aa50825c02a3e1ca0d1e4c5fa0b02ad2352bb2b1
Bzarre <32144764@dankook.ac.kr>
2024-11-21 20:47:40 +0900

    Utils Error

--- src/utils/error/errorHandler.js
+++ src/utils/error/errorHandler.js
@@ -1,1 +1,21 @@
-export const handleError = ()  => {};
+import { ErrorCodes } from './errorCodes.js';
+
+export const handleError = (socket, error) => {
+  try {
+    let responseCode;
+    let message;
+    if (error.code) {
+      responseCode = error.code;
+      message = error.message;
+      const err = `${error.name}\r\nCode: ${error.code}\r\nMessage: ${error.message}\r\n${error.stack}`;
+      console.error(err);
+    } else {
+      responseCode = ErrorCodes.SOCKET_ERROR;
+      message = error.message;
+      const err = `${error.name}\r\nMessage: ${error.message}\r\n${error.stack}`;
+      console.error(err);
+    }
+  } catch (err) {
+    console.error('Error in errorHandler:', err);
+  }
+};
