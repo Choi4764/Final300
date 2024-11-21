76369b48ec9d77110af628920fb7f1ce5f2ad0f3
Bzarre <32144764@dankook.ac.kr>
2024-11-21 10:36:57 +0900

    주석

--- src/handler/index.js
+++ src/handler/index.js
@@ -3,6 +3,7 @@ import { registerHandler } from './user/registerHandler.js'
 import { PACKET_TYPE } from '../constants/header.js';
 
 const handlers = {
+    //회원가입 및 로그인
     [PACKET_TYPE.REGISTER_REQUEST]: { handler: registerHandler },
     [PACKET_TYPE.LOGIN_REQUEST]: { handler: loginHandler }
 
