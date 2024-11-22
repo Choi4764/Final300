0efb00ac10b2ac20fe5887a3884eaa23ee3617bd
Bzarre <32144764@dankook.ac.kr>
2024-11-22 09:42:22 +0900

    변수명

--- src/handler/town/chat.handler.js
+++ src/handler/town/chat.handler.js
@@ -3,12 +3,12 @@ import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { handleError } from '../../utils/error/errorHandler.js';
 
 export const chatHandler = ({ socket, payload }) => {
-    const { nickname, chatContext } = payload;
+    const { playerId, chatContext } = payload;
 
     try {
         const { chatContext } = packet;
 
-        const user = getUserById(nickname);
+        const user = getUserById(playerId);
         if (!user) {
           throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
         }
@@ -18,7 +18,7 @@ export const chatHandler = ({ socket, payload }) => {
           throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '세션을 찾을 수 없습니다.');
         }
     
-        gameSession.chatPlayer(nickname, chatContext);
+        gameSession.chatPlayer(playerId, chatContext);
       } catch (error) {
         handleError(socket, error);
     }
