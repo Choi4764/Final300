2abf0cd44b4656bf30a443ade512564b3a526d9b
Bzarre <32144764@dankook.ac.kr>
2024-11-27 11:18:16 +0900

    일부 수정

--- src/handler/character/chat.handler.js
+++ src/handler/character/chat.handler.js
@@ -3,7 +3,8 @@ import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { handleError } from '../../utils/error/errorHandler.js';
 import { getAllUsers, getUserById } from '../../sessions/user.session.js';
 import { getGameSession } from '../../sessions/game.session.js';
-import { TOWN_SESSION_ID } from '../../constants/session.js';
+import { townSession } from '../../sessions/sessions.js';
+import sendResponsePacket from '../../utils/response/createResponse.js';
 
 export const ChatHandler = async ({ socket, payload }) => {
     const { playerId, chatContext } = payload;
@@ -12,7 +13,7 @@ export const ChatHandler = async ({ socket, payload }) => {
         const sender = await getUserById(playerId);
         if (!sender) throw new Error('player not found');
 
-        const gameSession = getGameSession(TOWN_SESSION_ID);
+        const gameSession = getGameSession(townSession);
         if(!gameSession) throw new Error('session not found');
 
         if(chatContext[0] === '/'){//명령어 부분 추후 작성
@@ -30,7 +31,7 @@ export async function chatAll(sender, context) {
     try{
         const allUsers = getAllUsers();
 
-        const chatResponse = createResponse('response', 'S_ChatResponse', {
+        const chatResponse = sendResponsePacket(PACKET_TYPE.S_ChatResponse, {
           playerId: sender.playerId,
           chatContext: `${sender.nickname}: ${context}`
         });
